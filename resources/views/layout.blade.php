2ed50e5599a05f0f2f42cc12dd7dd3d7004439b2
Manuel Camacho <maedca@gmail.com>
2016-01-19 16:21:39 -0430

    clase 19/1

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -39,7 +39,7 @@
 
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-                    {!! Html::menu() !!}
+                    {!! Html::menu('test.menu') !!}
 
 
                     <ul class="nav navbar-nav navbar-right">
--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,14 +1,7 @@
 <ul class="nav navbar-nav">
-    <li role="presentation" @if(Route::is('tickets.latest')) class="active" @endif>
-        <a href={{route('tickets.latest')}}>Recientes</a>
-    </li>
-    <li role="presentation" @if(Route::is('tickets.popular')) class="active" @endif>
-        <a href={{route('tickets.popular')}}>Populares</a>
-    </li>
-    <li role="presentation" @if(Route::is('tickets.open')) class="active" @endif>
-        <a href={{route('tickets.open')}}>Abiertas</a>
-    </li>
-    <li role="presentation"@if(Route::is('tickets.closed')) class="active" @endif>
-        <a href={{route('tickets.closed')}}>Finalizadas</a>
-    </li>
+   @foreach($items as $route => $text)
+        <li role="presentation" {!! Html::classes(['active' => Route::is($route)]) !!}>
+            <a href="{{route($route)}}">{{ $text }}</a>
+       </li>
+       @endforeach
 </ul>
